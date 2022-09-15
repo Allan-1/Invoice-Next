fdc143268d05964190a324cba3e8c8aae8a01cc8
Allan Muturi <amuturi01@gmail.com>
2022-09-15 19:22:05 +0300

    add invoice provider to _app.js

--- components/InvoiceList/InvoiceList.js
+++ components/InvoiceList/InvoiceList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import { FaCaretRight } from 'react-icons/fa';
 import styles from './styles/invoicelist.module.css';
 import useInvoice from '../../context/InvoiceContext';
@@ -11,21 +12,23 @@ function InvoiceList() {
       <div className={styles.invoicelist}>
         <div>
           {invoices.map((data) => (
-            <div className={styles.invoicedata} key={data.invoiceno}>
-              <div className={styles.invoicedatauser}>
-                <div className={styles.invoiceno}>
-                  <span className={styles.sign}>#</span>
-                  {data.invoiceno}
+            <Link href={`/invoice/${data.invoiceno}`} key={data.invoiceno}>
+              <div className={styles.invoicedata}>
+                <div className={styles.invoicedatauser}>
+                  <div className={styles.invoiceno}>
+                    <span className={styles.sign}>#</span>
+                    {data.invoiceno}
+                  </div>
+                  <div className={styles.todane}>{data.invoicedate} </div>
+                  <div className={styles.todane}>{data.toname}</div>
                 </div>
-                <div className={styles.todane}>{data.invoicedate} </div>
-                <div className={styles.todane}>{data.toname}</div>
+                <div className={styles.price}>Ksh {addItems(data.items)}</div>
+                <div className={styles.status}>{data.status}</div>
+                <div className={styles.caret}>
+                  <FaCaretRight />
+                </div>
               </div>
-              <div className={styles.price}>Ksh {addItems(data.items)}</div>
-              <div className={styles.status}>{data.status}</div>
-              <div className={styles.caret}>
-                <FaCaretRight />
-              </div>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
